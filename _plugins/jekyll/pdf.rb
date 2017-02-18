257636d003ad0f0a5161c296f01dc7df5c4e14cc
napalm255 <napalm255@gmail.com>
2017-02-17 23:00:57 -0500

    updated resume

--- _plugins/jekyll/pdf.rb
+++
@@ -1,1 +0,0 @@
-Dir[File.dirname(__FILE__) + '/pdf/**/*.rb'].each {|file| require file }
--- _plugins/jekyll/pdf/helper.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module PDF
-    module Helper
-      def fix_relative_paths
-        output.gsub!(/(href|src)=(['"])\/([^\/"']([^\"']*|[^"']*))?['"]/, "\\1=\\2file://#{site.dest}/\\3\\2") if output != nil
-      end
-    end
-  end
-end
